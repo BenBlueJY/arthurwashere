302f5ad2f196d4c61c741e68cb8658886c4ea1b2
Illuminum666 <arthur1chan@gmail.com>
2022-06-22 12:21:20 -0400

    hmm

--- hashTable.c
+++ hashTable.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-<<<<<<< HEAD
-#define HASH_TABLE_SIZE 500
-=======
 #define HASH_TABLE_SIZE 9000
->>>>>>> 33436a45d7a98d9aa89491aae62591ba872e26cb
 
 char hashTable[HASH_TABLE_SIZE];
 
