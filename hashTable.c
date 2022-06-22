45262f42aee32e20ade398d6cecd211b8d7deca8
Illuminum666 <arthur1chan@gmail.com>
2022-06-22 11:34:04 -0400

    test

---
+++ hashTable.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define HASH_TABLE_SIZE 1000
+
+char hashTable[HASH_TABLE_SIZE];
+
+int hashFunction(char* s) {
+    return atoi(s) % HASH_TABLE_SIZE;
+}
+
+void insert(char* s) {
+    int index = hashFunction(s);
+    while (hashTable[index] != '') {
+        index = (index + 1) % HASH_TABLE_SIZE;
+    }
+    hashTable[index] = s;
+}
